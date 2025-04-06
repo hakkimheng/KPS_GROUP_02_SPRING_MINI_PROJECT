e32d0aeb49127ca15c4f1eb690d18ac755b02461
Kimheng HAK <110073572+hakkimheng@users.noreply.github.com>
2025-04-06 10:24:06 +0700

    add profile dto commit

--- src/main/java/springkpsgroup02/kps/Model/Entity/Profile.java
+++ src/main/java/springkpsgroup02/kps/Model/Entity/Profile.java
@@ -1,5 +1,23 @@
 package springkpsgroup02.kps.Model.Entity;
 
-public class Profile {
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
 
+import java.util.Date;
+import java.util.UUID;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class Profile {
+    private UUID id;
+    private String username;
+    private String email;
+    private String password;
+    private Integer level;
+    private Integer xp;
+    private String profileImage;
+    private Boolean isVerified;
+    private Date createdAt;
 }
